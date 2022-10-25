40f39ec69dcf1d11677adf0143fe4126fd69c3cd
David Martini <88782651+davmartini@users.noreply.github.com>
2022-10-25 12:23:30 +0200

    Update index.php

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -94,7 +94,7 @@
           <div style="text-align:center">
             <img src="images/atos.png" width="400" height="100"/>
           </div>
-          <div style="background-color:red;color:white;text-align:center">
+          <div style="background-color:blue;color:white;text-align:center">
             <p>Elancourt</p>
           </div>
           <hr/>
