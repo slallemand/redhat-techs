2c2163fc23879e4ccf3b65cb09d002a32a763db3
davmartini <88782651+davmartini@users.noreply.github.com>
2022-11-21 14:21:11 +0100

    change title

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -71,7 +71,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">OpenShift Hello World</a>
+              <a class="navbar-brand" href="#">OpenShift s2i</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
