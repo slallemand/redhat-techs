09c21ccf3439e8dcabdcda6403ec16a5e02d5412
David Martini <88782651+davmartini@users.noreply.github.com>
2023-06-07 09:47:31 +0200

    Update index.php

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -92,7 +92,7 @@
             <h3><?php echo "Container ID : ".gethostname(); ?></h3>
           </div>
           <div style="text-align:center">
-            <img src="images/scc.png" width="500" height="281"/>
+            <img src="images/scc.png" width="1145" height="157"/>
           </div>
           <div style="background-color:blue;color:white;text-align:center">
             <p>Paris</p>
