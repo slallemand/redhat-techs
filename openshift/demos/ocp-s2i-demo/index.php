762abe433c7006644060354dbf716eeb2a026697
David Martini <88782651+davmartini@users.noreply.github.com>
2023-06-08 11:43:31 +0200

    Update index.php

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -94,7 +94,7 @@
           <div style="text-align:center">
             <img src="images/redhat.png" width="1145" height="157"/>
           </div>
-          <div style="background-color:red;color:white;text-align:center">
+          <div style="background-color:blue;color:white;text-align:center">
             <p>Paris</p>
           </div>
           <hr/>
