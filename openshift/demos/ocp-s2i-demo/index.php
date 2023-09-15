484a1d74bd74029aec585b5cba0d73e3b024e6cb
David Martini <88782651+davmartini@users.noreply.github.com>
2023-09-15 11:42:35 +0200

    Update index.php

--- openshift/demos/ocp-s2i-demo/index.php
+++ openshift/demos/ocp-s2i-demo/index.php
@@ -94,7 +94,7 @@
           <div style="text-align:center">
             <img src="images/think.png" width="500" height="281"/>
           </div>
-          <div style="background-color:blue;color:white;text-align:center">
+          <div style="background-color:red;color:white;text-align:center">
             <p>Lille</p>
           </div>
           <hr/>
